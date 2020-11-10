8eaabf8792e7b4393fa1fe94036690f5d2b5d45d
Sebastien Danthinne <sdanthinne@gmail.com>
2020-11-10 00:46:47 +0000

    added important notes about the syscall required

--- examples/describe_events.c
+++ examples/describe_events.c
@@ -3,12 +3,26 @@
 #include <string.h>
 #include <stdlib.h>
 #include <err.h>
+#include <syscall.h>
+#include <perfmon.h>
 
+/*
+ *So important things:
+ * /usr/include/linux/perf_event.h < contains necessary structs for hte perf_event_open syscall
+ * so we need to look at the perf_event_open syscall - not even sure if it is neccary to use libpfm?
+ *
+ * */
 void list_pmu_events(pfm_pmu_t pmu)
 {
    pfm_event_info_t info;
    pfm_pmu_info_t pinfo;
-   int i, ret;
+   pfm_event_attr_info_t info_s_event;
+
+   int i,j, ret;
+   pfarc_context_t ctxt;
+   memset(&ctxt,0,sizeof(ctxt));
+   long my_cmd = perfmonctl(0,PFM_CREATE_CONTEXT, ctxt,1);   
+
    memset(&info, 0, sizeof(info));
    memset(&pinfo, 0, sizeof(pinfo));
    info.size = sizeof(info);
@@ -24,9 +38,18 @@ void list_pmu_events(pfm_pmu_t pmu)
       ret = pfm_get_event_info(i,PFM_OS_NONE, &info);
       if (ret != PFM_SUCCESS)
         errx(1, "cannot get event info");
-        printf("%s Event: %s::%s\n",
+      for(j=0;j<info.nattrs;j++)
+      {
+        if(pfm_get_event_attr_info(i,j,PFM_OS_NONE,&info_s_event)!= PFM_SUCCESS)
+        {
+            errx(1,"cannot get info attributes");
+        }
+        printf(" attr: %s : %s : %s\n",info.name,info_s_event.name,info_s_event.desc);
+      }
+      
+      printf("%s Event: %s::%s : %d\n",
                pinfo.is_present ? "Active" : "Supported",
-               pinfo.name, info.name);
+               pinfo.name, info.name,info.nattrs);
   }
 }
 
@@ -34,4 +57,5 @@ void main()
 {
     pfm_initialize();
     list_pmu_events(PFM_PMU_ARM_CORTEX_A53);
+    //list_pmu_events(PFM_PMU_ARM_CORTEX_A53);
 }
